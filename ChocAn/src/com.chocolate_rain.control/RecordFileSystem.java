6eb046b763987a8e764b6c97f9f2916ca102a5d8
Scott <sweidenkopf@gmail.com>
2014-11-15 22:48:52 -0500

    RecordFileSystem I
    Created method stubs, enumerated type called Type (for record and report
    types).

--- ChocAn/src/com.chocolate_rain.control/RecordFileSystem.java
+++ ChocAn/src/com.chocolate_rain.control/RecordFileSystem.java
@@ -1,9 +1,78 @@
 package com.chocolate_rain.control;
 
+import java.math.BigDecimal;
+
+import com.chocolate_rain.provider_directory.ProviderDirectory;
+import com.chocolate_rain.report.Report;
+
+
+
 public class RecordFileSystem {
-
-	public RecordFileSystem() {
-		// TODO Auto-generated constructor stub
+	
+	public static enum Type{
+		PROVIDER, MEMBER, EFT, SERVICE, SUMMARY
 	}
-
+	
+	private RecordMaintainer maintainer;
+	private ReportGenerator generator;
+	private ProviderDirectory directory;
+	
+	public RecordFileSystem() {
+		//TODO
+	}
+	
+	public boolean lookUpService(String serviceCode){
+		//return directory.getEntry(serviceCode); TODO
+		return true;
+	}
+	
+	public boolean createRecord(Type type, String[] personalInfo){
+		//return maintainer.createRecord(type, personalInfo); TODO
+		return true;
+	}
+	
+	public boolean updateRecord(Type type, String key, String field, String value){
+		//return maintainer.updateRecord(String type, String key, String field, String value); TODO
+		return true;
+	}
+	
+	public boolean deleteRecord(Type type, String key){
+		//return maintainer.deleteRecord(type, key); TODO
+		return true;
+	}
+	
+	public Report requestReport(Type type, String number){
+		switch(type){
+		case MEMBER:
+			//generator.generateReport(maintainer.getMember(number), maintainer.getServiceRecords()) TODO
+			break;
+		case PROVIDER:
+			//generator.generateReport(maintainer.getProvider(number), maintainer.getServiceRecords()) TODO
+			break;
+		default:
+			return null;
+		}
+		return null;
+	}
+	
+	public ProviderDirectory requestProviderDirectory(){
+		return directory;
+	}
+	
+	public boolean updateProviderDirectory(String serviceCode, ProviderDirectory.Fields field, Object value){
+		//providerDirectory.updateEntry(serviceCode, field, value)TODO
+		return true;
+	}
+	
+	public boolean addProviderDirectoryEntry(String serviceCode, String serviceName, BigDecimal serviceCost){
+		//providerDirectory.addEntry(String serviceCode, String serviceName, BigDecimal serviceCost)TODO
+		return true;
+	}
+	
+	public String validate(Type type, String identifier){
+		//return maintainer.validateAgainst(type, identifier);
+		//TODO
+		return null;
+	}
+	
 }
--- ChocAn/src/com.chocolate_rain.provider_directory/ProviderDirectory.java
+++ ChocAn/src/com.chocolate_rain.provider_directory/ProviderDirectory.java
@@ -2,6 +2,10 @@ package com.chocolate_rain.provider_directory;
 
 public class ProviderDirectory {
 
+	public static enum Fields{
+		SERVICE_CODE, SERVICE_NAME, SERVICE_PRICE
+	}
+	
 	public ProviderDirectory() {
 		// TODO Auto-generated constructor stub
 	}
